d40343a395dcf61a35237a6a4521d10e725906fc
qinyu.zhao <qinyu.zhao@sophgo.com>
2023-04-14 18:02:53 +0800

    test_runtime add ARM64 and X64 runner

--- test/test_nntc.py
+++ test/test_nntc.py
@@ -22,17 +22,6 @@ def make_lmdb(nntc_env):
 @pytest.mark.build
 @pytest.mark.nntc
 @pytest.mark.parametrize('target', ['BM1684', 'BM1684X'])
-def test_nntc_accuracy(target, nntc_env, get_imagenet_val, get_cifar100, get_coco2017_val, make_lmdb):
-    if not nntc_env['case_list']:
-        logging.info(f'Skip nntc accuracy test')
-        return
-
-    # Build for accuracy test
-    container_run(nntc_env, f'python3 -m tpu_perf.build {nntc_env["case_list"]} --outdir out_acc_{target} --target {target}')
-
-@pytest.mark.build
-@pytest.mark.nntc
-@pytest.mark.parametrize('target', ['BM1684', 'BM1684X'])
 def test_nntc_efficiency(target, nntc_env, get_imagenet_val, get_cifar100, get_coco2017_val):
     if not nntc_env['case_list']:
         logging.info(f'Skip nntc efficiency test')
@@ -40,3 +29,14 @@ def test_nntc_efficiency(target, nntc_env, get_imagenet_val, get_cifar100, get_c
 
     # Build for efficiency test
     container_run(nntc_env, f'python3 -m tpu_perf.build --time {nntc_env["case_list"]} --outdir out_eff_{target} --target {target}')
+
+@pytest.mark.build
+@pytest.mark.nntc
+@pytest.mark.parametrize('target', ['BM1684', 'BM1684X'])
+def test_nntc_accuracy(target, nntc_env, get_imagenet_val, get_cifar100, get_coco2017_val, make_lmdb):
+    if not nntc_env['case_list']:
+        logging.info(f'Skip nntc accuracy test')
+        return
+
+    # Build for accuracy test
+    container_run(nntc_env, f'python3 -m tpu_perf.build {nntc_env["case_list"]} --outdir out_acc_{target} --target {target}')
